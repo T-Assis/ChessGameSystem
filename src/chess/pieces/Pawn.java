2c5546b88244df838ab63ca99b0d5940629d5c47
Thales Lima de Assis <thaleslima-19@hotmail.com>
2018-12-21 14:24:03 -0200

    Bishop

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -17,15 +17,15 @@ public class Pawn extends ChessPiece {
 		
 		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position auxPosition = new Position(0, 0);
-		
+
 		if (getColor() == Color.WHITE) {
 			auxPosition.setValues(position.getRow() - 1, position.getColumn());
 			if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition)) {
 				matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
 			}
 			auxPosition.setValues(position.getRow() - 2, position.getColumn());
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());
-			if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+			Position auxPosition2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(auxPosition) && !getBoard().thereIsAPiece(auxPosition) && getBoard().positionExists(auxPosition2) && !getBoard().thereIsAPiece(auxPosition2) && getMoveCount() == 0) {
 				matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
 			}
 			auxPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -58,6 +58,7 @@ public class Pawn extends ChessPiece {
 		}
 		return matrix;
 	}
+	
 	@Override
 	public String toString(){
 		return "P";
